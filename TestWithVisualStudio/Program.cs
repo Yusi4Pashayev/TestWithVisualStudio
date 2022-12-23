9dbccd2bbd168e2d3e5ac65eaeca2a2420fc28db
Yusif <yusifyp@code.edu.az>
2022-12-23 17:57:52 +0400

    Add project files.

---
+++ TestWithVisualStudio/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestWithVisualStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Test With Visual Studio");
+        }
+    }
+}
